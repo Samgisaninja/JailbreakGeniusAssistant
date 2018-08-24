b1ce608472f558d40c478c07ca258130d99f3320
Sam Gardner <stgardner4@att.net>
2018-08-24 13:49:17 -0500

    Removes those pesky prefixes

--- jailbreakgeniusassistant/ViewController.m
+++ jailbreakgeniusassistant/ViewController.m
@@ -23,12 +23,17 @@
 -(IBAction)runCheck:(id)sender{
     NSString *statusFile = [[NSString alloc]initWithContentsOfFile:@"/Library/dpkg/status" encoding:NSUTF8StringEncoding error:nil];
     NSArray *installedPackages = [statusFile componentsSeparatedByString:@"\n"];
-    NSMutableArray *installedPackageBundleIDs = [[NSMutableArray alloc] init];
+    NSMutableArray *installedPackageBundleIDsWithPrefix = [[NSMutableArray alloc] init];
     int i;
     for (i=0; i < [installedPackages count]; i++) {
         if ([[installedPackages objectAtIndex:i] hasPrefix:@"Package:"]) {
-            [installedPackageBundleIDs addObject:[installedPackages objectAtIndex:i]];
+         [installedPackageBundleIDsWithPrefix addObject:[installedPackages objectAtIndex:i]];
         }
+    }
+    NSMutableArray * installedPackageBundleIDs = [[NSMutableArray alloc] init];
+    for (i=0; i < [installedPackageBundleIDsWithPrefix count]; i++) {
+        NSString *withoutPrefix = [[installedPackageBundleIDsWithPrefix objectAtIndex:i] stringByReplacingOccurrencesOfString:@"Package: " withString:@""];
+        [installedPackageBundleIDs addObject:withoutPrefix];
     }
     NSLog(@"GENIUS APP TESTING installedPackageBundleIDs: %@", installedPackageBundleIDs);
     [installedPackageBundleIDs writeToFile:@"/var/mobile/Media/installed_packages.plist" atomically:TRUE];
